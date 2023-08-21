8f73daf956587e3694c48a57d83b23060dbaa079
Steffen André Langnes <steffenl.sw@gmail.com>
2023-08-21 14:37:21 +0900

    Remove scripts

--- script/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-mkdir -p build/examples/go || true
-
-echo "Building Go examples"
-go build -o build/examples/go/basic examples/basic.go
-go build -o build/examples/go/bind examples/bind.go
